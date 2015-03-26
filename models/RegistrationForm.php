3f8b75fd5101c06a64c27c2faf2bcdf0e537611c
Ilya <bugsbunni@yandex.ru>
2015-03-26 18:06:41 +0500

    форма регистрации

--- models/RegistrationForm.php
+++ models/RegistrationForm.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * RegistrationForm is the model behind the registration form.
+ */
+class RegistrationForm extends Model
+{
+	public $username;
+	public $email;
+	public $password;
+	
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+				'username' => 'Логин',
+				'email' => 'Почта',
+				'password' => 'Пароль'
+		);
+	}
+}
