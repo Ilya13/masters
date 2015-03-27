cdbc10061e161bf35ed99def70e7d4b328b0fe02
Ilya <bugsbunni@yandex.ru>
2015-03-27 18:19:37 +0500

    Доработка навигатора

--- models/Category.php
+++ models/Category.php
@@ -4,6 +4,13 @@ namespace app\models;
 
 class Category extends \yii\db\ActiveRecord
 {
+	private $items = array();
+	
+	public function getItems()
+	{
+		return $this->items;
+	}
+	
 	public static function getNavigatorCategories()
 	{
 		return parent::find()->where(['active' => 1, 'navigator' => 1])->all();
@@ -12,5 +19,10 @@ class Category extends \yii\db\ActiveRecord
 	public static function tableName()
 	{
 		return 'categories';
+	}
+	
+	public function addItem($item)
+	{
+		rray_push($this->items, $item);
 	}
 }
